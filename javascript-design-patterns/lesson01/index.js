66753009611063b4213c1220928520f835fe3f7d
etczrn <etczrn@gmail.com>
2022-04-10 14:08:25 +0900

    Delete unnecessary files

--- javascript-design-patterns/lesson01/index.js
+++
@@ -1,7 +0,0 @@
-const catNames = ["Oliver", "Leo", "Milo", "Charlie", "Simba"];
-
-const cats = [
-  { name: "Oliver", image: "/images/alexander-tsang-lps5cLZ6j2g-unsplash.jpg" },
-];
-
-const content = document.getElementById("content");
