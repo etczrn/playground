22896d1efb9fb541556f432069369281b6d4d65c
etczrn <etczrn@gmail.com>
2022-04-15 22:17:15 +0900

    Add full screen option

--- javascript30/11 - Custom Video Player/scripts.js
+++ javascript30/11 - Custom Video Player/scripts.js
@@ -59,3 +59,11 @@ progress.addEventListener("click", scrub);
 progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
 progress.addEventListener("mousedown", () => (mousedown = true));
 progress.addEventListener("mouseup", () => (mousedown = false));
+
+// full screen
+const fullScreenBtn = document.querySelector(".full-screen");
+fullScreenBtn.addEventListener("click", () => {
+  if (!document.fullscreenElement) {
+    video.requestFullscreen();
+  }
+});
