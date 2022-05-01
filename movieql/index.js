2acf641fee96966ff0b5119115323cdc2f4d6f49
etczrn <etczrn@gmail.com>
2022-05-01 20:45:00 +0900

    #0.5 Creating the first Query and Resolver

--- movieql/index.js
+++ movieql/index.js
@@ -1,7 +1,9 @@
 import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
 
-// console.log("I'm testing...");
-
-const server = new GraphQLServer({});
+const server = new GraphQLServer({
+  typeDefs: "graphql/schema.graphql",
+  resolvers,
+});
 
 server.start(() => console.log("GraphQL Server running..."));
