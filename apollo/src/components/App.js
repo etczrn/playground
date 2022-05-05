21c831f1dc3e94965498f7f53a6bda3bb194f03d
etczrn <etczrn@gmail.com>
2022-05-05 20:55:47 +0900

    #0.4 Router and Styles

--- apollo/src/components/App.js
+++ apollo/src/components/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <></>;
-}
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "../routes/detail";
+import Home from "../routes/home";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
