cf79707eeea2128fbfd04fa2829fae86207bac9a
etczrn <etczrn@gmail.com>
2022-05-07 20:07:28 +0900

    #1.1 GET MOVIES Query

--- apollo/src/routes/home.js
+++ apollo/src/routes/home.js
@@ -1,1 +1,25 @@
-export default () => "home";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
+
+  if (loading) {
+    return "Loading...";
+  }
+
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1>{m.id}</h1>);
+  }
+
+  return <h1>Home</h1>;
+};
